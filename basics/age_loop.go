86bb94a0c4f24518fa24fab38b2e156ae19663d5
Raitis Daniels Zvīdriņš <raitis.zv@proton.me>
2025-12-28 13:18:29 +0200

    Complete Day 2 age classification with indexed loop

--- basics/age_loop.go
+++ basics/age_loop.go
@@ -3,19 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	ages := []int{10, 15, 23, 40}
+	ages := []int{10, 15, 18, 23, 40, 65}
 
-	for _, age := range ages {
+	var personNum = 1
+
+	for index, age := range ages {
+		personNum = index + 1
 		var status string
 
-		if age < 13 {
-			status = "Child"
+		if age >= 65 {
+			status = "a senior"
+		} else if age < 13 {
+			status = "a child"
 		} else if age < 18 {
-			status = "Teenager"
+			status = "a teenager"
 		} else {
-			status = "Adult"
+			status = "an adult"
 		}
 
-		fmt.Printf("Age %d -> %s \n", age, status)
+		fmt.Printf("The person #%d is %s, age: %d\n", personNum, status, age)
 	}
 }
