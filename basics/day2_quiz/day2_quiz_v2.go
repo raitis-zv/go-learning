d2b717c96a0761d95db8793d5bb490730f657743
Raitis Daniels Zvīdriņš <raitis.zv@proton.me>
2025-12-29 23:10:11 +0200

    Refactor quiz to minimal readable implementation

--- basics/day2_quiz/day2_quiz_v2.go
+++ basics/day2_quiz/day2_quiz_v2.go
@@ -5,61 +5,52 @@ import (
 	"strconv"
 )
 
+func waitInput() {
+	fmt.Println("Press Enter to continue...")
+	fmt.Scanln()
+}
+
 func main() {
 	fmt.Println("___ MATH QUIZ ___\n")
 	fmt.Println("Rules:")
 	fmt.Println("1. Get 1 point for each correct answer")
 	fmt.Println("2. Enter only whole numbers for the answers")
 	fmt.Println("3. One try per question\n")
-	fmt.Println("Press Enter to continue...")
-	fmt.Scanln()
+	waitInput()
 
 	var totalPoints = questionCheck()
-	var incorrectAnsw = 5 - totalPoints
-	var totalScore = (5 - incorrectAnsw) * 20
 
-	fmt.Printf("Your score: %d%%\n", totalScore)
+	fmt.Printf("Your score: %d%%\n", totalPoints*20)
 	fmt.Printf("Correct answers: %v\n", totalPoints)
-	fmt.Printf("Incorrect answers: %v\n", incorrectAnsw)
+	fmt.Printf("Incorrect answers: %v\n", 5-totalPoints)
 }
 
 func questionCheck() int {
 	var points int
-	var answer string
-	var userAnswer int
+	var userAnswer string
 
-	questions := []string{
-		"What is 6 x 6?",
-		"What is 99 + 0 ?",
-		"What is 10 / 10?",
-		"What is 42 + 1?",
-		"What is 100 - 67?"}
+	qOperations := []string{"6 x 6", "99 + 0", "10 / 10", "42 + 1", "100 - 67"}
 
-	answers := []int{36, 99, 1, 43, 33}
+	answers := []string{"36", "99", "1", "43", "33"}
 
-	for i, q := range questions {
+	for i := range qOperations {
 		for {
-			fmt.Printf("Question %d: %s\n", i+1, q)
-			fmt.Scanln(&answer)
-			temp, err := strconv.Atoi(answer)
+			fmt.Printf("Question %d: What is %s?\n", i+1, qOperations[i])
+			fmt.Scanln(&userAnswer)
+			temp, err := strconv.Atoi(userAnswer)
 
 			if err != nil || temp < 0 {
 				fmt.Println("Please enter a positive whole number")
 				continue
+			} else if userAnswer == answers[i] {
+				fmt.Println("Correct!")
+				points++
+			} else {
+				fmt.Println("Incorrect :(")
 			}
-			userAnswer = temp
 			break
 		}
-
-		if userAnswer == answers[i] {
-			fmt.Println("Correct!")
-			points++
-		} else {
-			fmt.Println("Incorrect :(")
-		}
-
-		fmt.Println("Press Enter to continue...")
-		fmt.Scanln()
+		waitInput()
 	}
 
 	return points
